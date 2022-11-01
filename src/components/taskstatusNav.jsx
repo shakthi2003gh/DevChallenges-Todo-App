4b7c4550fff41ca01d9fa37e5c16cf8182c57a06
shakthi <shakthikumar123456789@gmail.com>
2022-11-01 19:51:33 +0530

    Create task navigation section

---
+++ src/components/taskstatusNav.jsx
@@ -0,0 +1,11 @@
+const TodoNavigation = () => {
+  return (
+    <nav className="todo-navigation">
+      <span className="active">All</span>
+      <span>Active</span>
+      <span>Completed</span>
+    </nav>
+  );
+};
+
+export default TodoNavigation;
